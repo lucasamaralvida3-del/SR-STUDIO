fe57862d39f4b65cbba75001c97a02f7609b3a70
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-13 17:43:01 -0300

    Adiciona compatibilidade PPTX do Engine Beta 11

---
+++ staging/beta11/source/EncartesPPTX.py
@@ -0,0 +1,4 @@
+from EncartesAssets import save_font, list_fonts, font_path, asset_path
+
+def parse_pptx(data, source_name='modelo.pptx'):
+    raise RuntimeError('A importação PPTX da Beta 11 é processada localmente pelo novo editor.')
