950caf7df236de2cd3aec53c67672d6b128469cd
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-14 13:54:38 -0300

    Cria camada modules da arquitetura 5.0

---
+++ src/sr_studio/modules/__init__.py
@@ -0,0 +1,5 @@
+"""Módulos de negócio do SR Studio 5.0.
+
+Cartazes, atacado, encartes, promoções, organizador, SR IA e CISS serão
+migrados gradualmente para esta camada mantendo compatibilidade.
+"""
