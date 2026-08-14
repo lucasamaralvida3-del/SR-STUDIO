85893659bb749df217026081ee9a3e5961242cff
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-14 13:54:30 -0300

    Cria camada services da arquitetura 5.0

---
+++ src/sr_studio/services/__init__.py
@@ -0,0 +1,5 @@
+"""Serviços reutilizáveis do SR Studio 5.0.
+
+Projetos, banco de produtos, importadores, imagens, exportação, autosave,
+backup e demais serviços compartilhados serão extraídos para esta camada.
+"""
