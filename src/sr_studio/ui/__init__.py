8d06fd7e7838290fd3c0d16a40aa77e0d04c2929
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-14 13:54:45 -0300

    Cria camada ui da arquitetura 5.0

---
+++ src/sr_studio/ui/__init__.py
@@ -0,0 +1,5 @@
+"""Interface compartilhada do SR Studio 5.0.
+
+Shell, menu, diálogos, notificações, validação, gerenciador de projetos e
+gerenciador de modelos serão centralizados nesta camada.
+"""
