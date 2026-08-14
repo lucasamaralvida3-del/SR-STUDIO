61209de134113b86877ecb011a21a4eaaac5e5a0
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-14 13:54:22 -0300

    Cria camada core da arquitetura 5.0

---
+++ src/sr_studio/core/__init__.py
@@ -0,0 +1,5 @@
+"""Núcleo compartilhado do SR Studio 5.0.
+
+Esta camada receberá configuração, eventos, versionamento, modelos de domínio
+e validações fundamentais durante a migração incremental da linha 4.x.
+"""
