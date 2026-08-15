7333e832037af2cc14faeba15b51559edb5b0499
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-15 07:10:54 -0300

    release: promote SR Studio 5 Professional to Beta 7

--- src/srstudio/__init__.py
+++ src/srstudio/__init__.py
@@ -1,4 +1,6 @@
 """SR Studio 5.0 Professional core package."""
 
-__version__ = "5.0.0-alpha.1"
-__channel__ = "development"
+__version__ = "5.0.0-beta.7"
+__channel__ = "beta"
+__release_label__ = "Beta 7 Professional"
+__distribution_version__ = "5.0.0-hybrid.beta7"
