8baa9ba604d3e5887c88aa3972b8c69865cbc311
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-15 22:20:21 -0300

    chore(graphics2): publica grupos e fingerprint no alpha.6

--- src/srstudio/graphics2/__init__.py
+++ src/srstudio/graphics2/__init__.py
@@ -28,12 +28,14 @@ from .model import (
 from .operations import GraphicsSession
 from .pdf_baseline import PdfBaselinePage, render_pdf_baselines
 from .pptx_fidelity import EmbeddedPptxFont, PptxFidelityReport, enhance_pptx_document
+from .pptx_groups import PptxGroupReport, rebuild_pptx_groups
 from .preflight import PreflightIssue, run_preflight
 from .quality import ProductionGateIssue, ProductionGateReport, inspect_production_gate, store_visual_fidelity
 from .qt_renderer import RenderReport, render_pdf, render_png
+from .scene_fingerprint import PageFingerprint, SceneFingerprint, fingerprint_document, store_scene_fingerprint
 
 ENGINE_NAME = "SR Graphics Engine"
-ENGINE_VERSION = "2.0.0-alpha.5"
+ENGINE_VERSION = "2.0.0-alpha.6"
 SCHEMA_VERSION = "srscene/2.0"
 
 __all__ = [
@@ -56,24 +58,30 @@ __all__ = [
     "ImportAuditIssue",
     "ImportAuditReport",
     "NodeKind",
+    "PageFingerprint",
     "PdfBaselinePage",
     "PptxFidelityReport",
+    "PptxGroupReport",
     "PreflightIssue",
     "ProductionGateIssue",
     "ProductionGateReport",
     "Rect",
     "RenderReport",
     "SCHEMA_VERSION",
+    "SceneFingerprint",
     "SmartSlot",
     "Transform",
     "audit_import",
     "compare_images",
     "enhance_pptx_document",
+    "fingerprint_document",
     "inspect_production_gate",
+    "rebuild_pptx_groups",
     "render_pdf",
     "render_pdf_baselines",
     "render_png",
     "run_preflight",
     "run_suite",
+    "store_scene_fingerprint",
     "store_visual_fidelity",
 ]
