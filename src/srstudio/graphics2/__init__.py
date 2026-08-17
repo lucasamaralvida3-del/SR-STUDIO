2ea10fd1c348f4bc737cfaea697949d92b33718e
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-17 13:14:13 -0300

    graphics2: bump engine to alpha.42

--- src/srstudio/graphics2/__init__.py
+++ src/srstudio/graphics2/__init__.py
@@ -99,7 +99,7 @@ from .studio_bridge import (
 from .saved_merge import analyze_saved_session_merge, resolve_saved_session_merge
 
 ENGINE_NAME = "SR Graphics Engine"
-ENGINE_VERSION = "2.0.0-alpha.41"
+ENGINE_VERSION = "2.0.0-alpha.42"
 SCHEMA_VERSION = "srscene/2.0"
 
 __all__ = [
