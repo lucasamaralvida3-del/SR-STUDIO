29efaa81aa975cfcb56a3764748a22b18633b5e6
lucasamaralvida3-del <lucasamaralvida3@gmail.com>
2026-08-17 08:08:39 -0300

    test: lock QPainter nowrap semantics to explicit line breaks

--- tests/test_graphics2_text_fit_policy.py
+++ tests/test_graphics2_text_fit_policy.py
@@ -1,4 +1,19 @@
-from srstudio.graphics2.qt_renderer import _should_fit_text
+from srstudio.graphics2.qt_renderer import _should_fit_text, _text_flags
+
+
+class _Qt:
+    AlignLeft = 0x0001
+    AlignRight = 0x0002
+    AlignHCenter = 0x0004
+    AlignTop = 0x0020
+    AlignBottom = 0x0040
+    AlignVCenter = 0x0080
+    TextSingleLine = 0x0100
+    TextWordWrap = 0x1000
+
+
+class _QtCore:
+    Qt = _Qt
 
 
 def test_nowrap_only_controls_wrapping_not_font_size():
@@ -11,3 +26,21 @@ def test_explicit_fit_inside_box_keeps_auto_fit_behavior():
 
 def test_priceblock_overflow_policy_can_shrink_only_when_needed():
     assert _should_fit_text({"nowrap": True, "semantic_fit_policy": "overflow_only"}) is True
+
+
+def test_nowrap_disables_automatic_wrap_without_forcing_single_line():
+    flags = _text_flags({"nowrap": True, "align": "left", "v_align": "top"}, _QtCore)
+
+    assert flags & _Qt.AlignLeft
+    assert flags & _Qt.AlignTop
+    assert not flags & _Qt.TextWordWrap
+    assert not flags & _Qt.TextSingleLine
+
+
+def test_wrapped_text_keeps_qpainter_word_wrap():
+    flags = _text_flags({"nowrap": False, "align": "center", "v_align": "center"}, _QtCore)
+
+    assert flags & _Qt.AlignHCenter
+    assert flags & _Qt.AlignVCenter
+    assert flags & _Qt.TextWordWrap
+    assert not flags & _Qt.TextSingleLine
